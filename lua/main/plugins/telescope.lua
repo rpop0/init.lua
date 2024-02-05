260efd4571ba25d7a155c6cd472aaf8698c2a5dd
Pop Rosian <poprosian@gmail.com>
2024-02-05 16:19:23 +0200

    Add venv to telescope ignore

--- lua/main/plugins/telescope.lua
+++ lua/main/plugins/telescope.lua
@@ -2,7 +2,7 @@ return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.5',
 
-    dependencies = { 
+    dependencies = {
         'nvim-lua/plenary.nvim',
         'BurntSushi/ripgrep'
     },
@@ -10,15 +10,16 @@ return {
     config = function()
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {}) 
+        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>ps', function()
             builtin.grep_string({ search = vim.fn.input("grep > ") });
         end)
 
-        require('telescope').setup{ 
-            defaults = { 
-                file_ignore_patterns = { 
-                    "node_modules" 
+        require('telescope').setup{
+            defaults = {
+                file_ignore_patterns = {
+                    "node_modules",
+                    "venv"
                 }
             }
         }
