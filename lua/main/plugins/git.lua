1b1f887a144adce42ff8b74536621a3b4e604a79
Pop Rosian <poprosian@gmail.com>
2024-02-12 19:04:23 +0200

    added stage hunk

--- lua/main/plugins/git.lua
+++ lua/main/plugins/git.lua
@@ -8,6 +8,7 @@ return {
                 vim.keymap.set('n', '<leader>hr', gs.reset_hunk)
                 vim.keymap.set('n', '<leader>tb', gs.toggle_current_line_blame)
                 vim.keymap.set('n', '<leader>hb', function() gs.blame_line{full=true} end)
+                vim.keymap.set('n', '<leader>hs', gs.stage_hunk)
 
                 -- Text objects
                 vim.keymap.set('o', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
